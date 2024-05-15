a13226383935da88636b71f0e25caa77db27ce5c
aseoaneo23 <aseoaneo22@fpcoruna.afundacion.org>
2024-05-15 19:09:03 +0200

    Estructura de ficheros básica

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    specPattern: "cypress/features/**/*.feature",
+    baseUrl: "https://www.saucedemo.com/v1/",
+    chromeWebSecurity: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+  },
+});
